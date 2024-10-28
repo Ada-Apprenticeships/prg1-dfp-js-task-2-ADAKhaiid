907deab534128a605096fcb9edb5bb5c0418a325
KhalidB <khalid.balesaria@ada.ac.uk>
2024-10-28 11:49:04 +0000

    Last Finishes

--- dfp.js
+++ dfp.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
 // The neccessary files for this task
-const inputFile = "datafile.csv"
-const outputFile = "outputFile,csv"
+const inputFile = "datafile.csv";
+const outputFile = "outputFile,csv";
 
 // Function to create the output file
 function parseFile (indata, outdata, delimiter = ';') {
@@ -23,23 +23,23 @@ function parseFile (indata, outdata, delimiter = ';') {
   const lines = data.split(/\n/).slice(1);
 
   // Creating a total of the reviews
-  let totalReviews = 0 
+  let totalReviews = 0;
   // Going through each review:
   for (let line of lines) {
     // Trims the line of white spaces and splits the Review and Positive/Negative remark:
     const trimmedLine = line.trim().split(delimiter);
     // Creates individual outputline in order of Positive/Negative;The review in 20 characters per line
-    const outputLine = `${trimmedLine[1].trim()}${delimiter}${trimmedLine[0].trim().substring(0,20)}\n`
+    const outputLine = `${trimmedLine[1].trim()}${delimiter}${trimmedLine[0].trim().substring(0,20)}\n`;
    
     // Outputting the file
     fs.appendFileSync(outdata, outputLine);
     // Adding to total reviews after each review
-    totalReviews++
+    totalReviews++;
   }
   return totalReviews
 }
 
-console.log(parseFile(inputFile, outputFile))
+console.log(parseFile(inputFile, outputFile));
 
 // Leave this code here for the automated tests
 module.exports = {
